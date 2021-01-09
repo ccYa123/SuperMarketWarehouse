ee932a9c88ccd91ab5505c7a5a8a420446494c18
19145 <1914520351@qq.com>
2021-01-09 19:26:05 +0800

    超市仓库管理系统

---
+++ src/Test/DbUtilTest.java
@@ -0,0 +1,31 @@
+package Test;
+
+import Util.DbUtil;
+import Util.Objects;
+import org.junit.Before;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DbUtilTest {
+
+    @org.junit.jupiter.api.Test
+    void addObjects() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteObjects() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void editMessages() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void selectObjects() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void judgeObjects() {
+    }
+}
